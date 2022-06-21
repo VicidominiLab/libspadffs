59005731cc86f63b435ffe28925207a627c5c88c
Mattia Donato <mattia.donato@iit.it>
2022-06-21 10:59:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import find_packages, setup
 from distutils.core import setup
-from pip.req import parse_requirements
-
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 print(find_packages())
 
