fad9dc624dcc3e0c8cfbf7965eafdba07d4a2fc0
Mattia Donato <mattia.donato@iit.it>
2022-06-21 11:01:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import find_packages, setup
 from distutils.core import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
 
 print(find_packages())
 
@@ -16,6 +12,10 @@ setup(
     author='Eli Slenders',
     author_email='eli.slenders@iit.it',
     description='The analysis of confocal laser-scanning microscopy based fluorescence fluctuation spectroscopy (FFS) data',
-    install_reqs = parse_requirements('requirements.txt', session='hack')
+    install_reqs = [
+                    "multipletau",
+                    "joblib",
+                    "h5py"
+                   ]
 )
 
